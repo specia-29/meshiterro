a7fd717f30d05ee86c0d8505fb87b4b6c7ed2f4e
EC2 Default User <specialized.19991129@gmail.com>
2022-09-11 01:03:17 +0000

    ~10章

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
+  get 'post_images/new'
+  get 'post_images/index'
+  get 'post_images/show'
+  root 'homes#top'
+  get '/about', to: 'homes#about'
+  devise_for :users
+
+  resources :post_images, only: [:new, :create, :index, :show]
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
