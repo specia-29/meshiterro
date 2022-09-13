af3af6e42f6d535f93a7a056fb110db9833bf3e1
EC2 Default User <specialized.19991129@gmail.com>
2022-09-13 19:09:40 +0900

    [~19章]

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,12 @@ Rails.application.routes.draw do
   get '/about', to: 'homes#about'
   devise_for :users
 
-  resources :post_images, only: [:new, :create, :index, :show, :destroy]
+  resources :post_images, only: [:new, :create, :index, :show, :destroy] do
+    resource :favorites, only: [:create, :destroy]
+
+    resources :post_comments, only: [:create, :destroy]
+  end
+
   resources :users, only: [:show, :edit, :update]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
